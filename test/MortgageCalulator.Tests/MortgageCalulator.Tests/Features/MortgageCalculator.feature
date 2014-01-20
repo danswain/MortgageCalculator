ee48b6ceff03d623edef0bec28256fd3cef1457e
Dan Swain <dan.swain@wonga.com>
2014-01-20 17:34:28 +0000

    initial test commit

---
+++ test/MortgageCalulator.Tests/MortgageCalulator.Tests/Features/MortgageCalculator.feature
@@ -0,0 +1,18 @@
+﻿Feature: MortgageCalculator
+	In order Calculate the total cost of a Mortgage over time
+	As a Existing homeowner who's up for a Remortgage
+	I want to be able to compare 2 mortgage offers over time.
+
+@mytag
+Scenario: Add two numbers
+	Given I want to borrow £228,000
+	And I want to repay it over 22 years
+	And I am comparing the following mortgages
+	| MortgageProvider | MortgageRate | MortgageTerm | ProductFee | MaximumAnnualOverpayments         |
+	| Nationwide       | 2.99%        | 22 years     | £995       | 10%                               |
+	| Yorkshire        | 2.84%        | 22 years     | £295       | 10% or £10,000 which ever is less |
+	When I click compare the mortgages
+	Then the result should be
+	| MortgageProvider | Year | Interest | Principal | Total    | Balance   |
+	| Nationwide       | 2014 | 6164.66  | 6811.41   | 12976.07 | 221188.59 | 
+	
---
+++ test/MortgageCalulator.Tests/MortgageCalulator.Tests/Steps/MortgageCalculatorSteps.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace MortgageCalulator.Tests.Steps
+{
+    [Binding]
+    public class MortgageCalculatorSteps
+    {
+        private decimal _principalAmount;
+        private int _loanTermInYears;
+
+        [Given(@"I want to borrow £(.*)")]
+        public void GivenIWantToBorrow(Decimal principalAmount)
+        {
+            _principalAmount = principalAmount;            
+        }
+
+        [Given(@"I want to repay it over (.*) years")]
+        public void GivenIWantToRepayItOverYears(int loanTermInYears)
+        {
+            _loanTermInYears = loanTermInYears;            
+        }
+
+        [Given(@"I am comparing the following mortgages")]
+        public void GivenIAmComparingTheFollowingMortgages(Table mortgagesToCompare)
+        {
+            mortgagesToCompare.Rows.Select(row => new
+            {
+                MortgageProvider = row["MortgageProvider"],
+
+            });
+        }
+
+        [When(@"I click compare the mortgages")]
+        public void WhenIClickCompareTheMortgages()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the result should be")]
+        public void ThenTheResultShouldBe(Table mortgageComparisonResult)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
